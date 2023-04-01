a43c7a03e74050aee973f4d554b1dec4a9c2c18f
fgbyte <garciabayonf@gmail.com>
2023-04-01 17:57:23 -0400

    site done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        brightRed:'hsl(12, 88%, 59%)',
+        brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
@@ -26,5 +26,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("daisyui")],
 };
